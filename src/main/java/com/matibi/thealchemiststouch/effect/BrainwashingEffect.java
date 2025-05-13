a69f3ca6cb71e1ac44037412c35836b493204e4b
Matibi <mathias.bagal@gmail.com>
2025-05-13 17:06:41 +0200

    adding long and strong potions variant

--- src/main/java/com/matibi/thealchemiststouch/effect/BrainwashingEffect.java
+++ src/main/java/com/matibi/thealchemiststouch/effect/BrainwashingEffect.java
@@ -1,6 +1,5 @@
 package com.matibi.thealchemiststouch.effect;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 

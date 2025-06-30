a3a87b378d0f93a02c0f490747aa0087be27588c
Matibi <mathias.bagal@gmail.com>
2025-06-30 12:08:14 +0200

    Added Alchemical Runes and Terrain Effects

--- src/main/java/com/matibi/thealchemiststouch/TheAlchemistsTouch.java
+++ src/main/java/com/matibi/thealchemiststouch/TheAlchemistsTouch.java
@@ -1,7 +1,6 @@
 package com.matibi.thealchemiststouch;
 
 import com.matibi.thealchemiststouch.effect.ModEffects;
-import com.matibi.thealchemiststouch.item.AlchemicalRuneItem;
 import com.matibi.thealchemiststouch.item.ModItems;
 import com.matibi.thealchemiststouch.potion.ModPotion;
 import net.fabricmc.api.ModInitializer;
